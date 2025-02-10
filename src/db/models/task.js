106a516ac03fe6f3b7487461cc7b1b1fa560bfe1
svichkar olga <syns41@gmail.com>
2025-02-10 18:39:25 +0200

    corr title in validation

--- src/db/models/task.js
+++ src/db/models/task.js
@@ -2,7 +2,7 @@ import { model, Schema } from 'mongoose';
 
 const taskSchema = new Schema(
   {
-    task: {
+    title: {
       type: String,
       required: true,
     },
--- src/validation/task.js
+++ src/validation/task.js
@@ -1,7 +1,7 @@
 import Joi from 'joi';
 
 export const taskSchema = Joi.object({
-  task: Joi.string().max(25).required(),
+  title: Joi.string().max(25).required(),
   description: Joi.string().max(50),
   columnName: Joi.string().valid('ToDo', 'In Progress', 'Done').default('ToDo'),
 });
